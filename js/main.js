f6e2c74206d9716a0159caadd6dd0d3c3c293364
southl <gyrii84@gmail.com>
2019-02-11 14:51:33 +0200

    embed video

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubePlayerAPIReady() {
+
+}
