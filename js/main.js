046305532d905812d3dee7c42fd8918779c84302
southl <gyrii84@gmail.com>
2019-02-12 21:48:28 +0200

    ass skills block

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
-// Inject YouTube API script
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+$(document).on('click', '.ourWork__button', function() {
+  var $video = $('#video'),
+    src = $video.attr('src');
+ 
+  $video.attr('src', src + '&autoplay=1');
+});
 
-function onYouTubePlayerAPIReady() {
 
-}
+//almost working variant
+
+// var tag = document.createElement('script');
+// tag.src = "//www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// var player;
+
+// // this function gets called when API is ready to use
+// function onYouTubePlayerAPIReady() {
+//   // create the global player from the specific iframe (#video)
+//   player = new YT.Player('video', {
+//      events: {
+//             'onStateChange': onPlayerStateChange
+//         }
+//   });
+// }
+
+// onPlayerStateChange = function (event) {
+//     if (event.data == YT.PlayerState.ENDED) {
+//         $('.ourWork__button').fadeIn('normal');
+//     }
+// }
+
+// $(document).on('click', '.ourWork__button', function () {
+//     $(this).fadeOut('normal');
+//     player.playVideo();
+// });
